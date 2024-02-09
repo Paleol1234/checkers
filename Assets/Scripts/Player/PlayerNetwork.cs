cc2815caeab4341349b1cc9cd2a9f9ef59be402f
Paleol <pavelpichkur@gmail.com>
2024-02-09 12:55:04 +0200

    Добавили отоброжения имя игрока

--- Assets/Scripts/Player/PlayerNetwork.cs
+++ Assets/Scripts/Player/PlayerNetwork.cs
@@ -7,5 +7,23 @@ using UnityEngine;
 
 public class PlayerNetwork : Player
 {
-    
+    [SyncVar]
+    private string displayName;
+    public string DisplayName
+    {
+        get
+        {
+            return displayName;
+        }
+        [Server]
+        set
+        {
+            if(value == "")
+            {
+                value = "default name";
+            }
+            displayName = value;
+        }
+    }    
 }
+
